bb809ec46beb7cd8968e210a1bbd51abc4f4dd4e
AyeenP <ayeen.pf@gmail.com>
2025-11-27 17:03:51 +0330

    feat: implement onsite payment method + main

--- Step-01-without-OOD/src/services/ReservationService.java
+++ Step-01-without-OOD/src/services/ReservationService.java
@@ -29,6 +29,9 @@ public class ReservationService {
             case CASH:
                 paymentProcessor.payByCash(res.totalPrice());
                 break;
+            case ONSITE:
+                paymentProcessor.payOnSite(res.totalPrice());
+                break;
         }
 
         System.out.println("----- INVOICE -----");
